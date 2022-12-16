2f93753a34dd9a5afcc0119881508b30f522be2d
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2022-12-17 01:39:39 +0900

    Update source.cpp

--- BOJ/코테대비/[No.17837]새로운 게임2/source.cpp
+++ BOJ/코테대비/[No.17837]새로운 게임2/source.cpp
@@ -8,134 +8,109 @@
 #include <string>
 using namespace std;
 
-struct Game {
+
+
+struct Mal {
     int x;
     int y;
-    int idx;
 };
 
-struct Infomation {
-    int bottom;
-    int top;
-    int cnt;
-};
-bool operator<(Game a, Game b) {
-    return a.idx > b.idx;
-}
+
+vector<int> info[13][13];
 int map[13][13];
-Infomation chess[13][13];
 int arrowMemo[11];
-priority_queue<Game> info;
+Mal gps[11];
 int mov[5][2] = { {0,0},{0,1},{0,-1},{-1,0},{1,0} };
 int N, K;
 
 
-bool isBlue(int nx, int ny) {
-    if (nx > N || nx <=0 || ny > N || ny <= 0 || map[nx][ny] == 2) {
+int Find(int x, int y, int idx) {
+    for (int a = 0; a < info[x][y].size(); ++a) {
+        if (info[x][y][a] == idx)
+            return a;
+    }
+}
+
+bool isBlue(int x, int y) {
+    if (x <= 0 || x > N || y > N || y <= 0 || map[x][y] == 2) {
         return 1;
     }
     return 0;
 }
-bool isRed(int nx, int ny) {
-    if (map[nx][ny] == 1)
+bool isRed(int x, int y) {
+    if (map[x][y] == 1)
+        return 1;
+    return 0;
+}
+bool isWhite(int x, int y) {
+    if (map[x][y] == 0)
         return 1;
     return 0;
 }
 
-bool isWhite(int nx, int ny) {
-    if (map[nx][ny] == 0)
-        return 1;
-    return 0;
-}
 
-void print() {
-    for (int a = 1; a <= N; ++a) {
-        for (int b = 1; b <= N; ++b) {
-            cout << "(" << chess[a][b].bottom << "," << chess[a][b].top << ","<<chess[a][b].cnt << ") ";
-        }
-        cout << "\n";
+void bokboot(int sourceX, int sourceY, int targetX, int targetY, int index) {
+    for (int a = index; a < info[sourceX][sourceY].size(); ++a) {
+        int idx = info[sourceX][sourceY][a];
+        gps[idx] = { targetX,targetY };
+        info[targetX][targetY].push_back(idx);
     }
-    cout << "\n";
+    info[sourceX][sourceY].erase(info[sourceX][sourceY].begin() + index, info[sourceX][sourceY].end());
 }
+
 int run() {
-
     int time = 1;
 
     while (time <= 1000) {
         int blueCnt = 0;
-        vector<Game> temp;
-        while (!info.empty()) {
-            Game now = info.top();
-            int count = chess[now.x][now.y].cnt;
-            info.pop();
-            
-            int arrow = arrowMemo[now.idx];
-            int nx = now.x + mov[arrow][0];
-            int ny = now.y + mov[arrow][1];
+        for (int a = 1; a <= K; ++a) {
+            int x = gps[a].x;
+            int y = gps[a].y;
+            int arrow = arrowMemo[a];
 
+            int idx = Find(x, y, a);
 
-            if (isBlue(nx,ny)) {
+            int nx = x + mov[arrow][0];
+            int ny = y + mov[arrow][1];
+
+            if (isBlue(nx, ny)) {
                 switch (arrow) {
-                case 1 :
-                    arrowMemo[now.idx] = 2;
+                case 1:
+                    arrowMemo[a] = 2;
                     break;
                 case 2:
-                    arrowMemo[now.idx] = 1;
+                    arrowMemo[a] = 1;
                     break;
-                case 3 :
-                    arrowMemo[now.idx] = 4;
+                case 3:
+                    arrowMemo[a] = 4;
                     break;
                 case 4:
-                    arrowMemo[now.idx] = 3;
+                    arrowMemo[a] = 3;
                     break;
                 }
-                arrow = arrowMemo[now.idx];
-                nx = now.x + mov[arrow][0];
-                ny = now.y + mov[arrow][1];
+                arrow = arrowMemo[a];
+                nx = x + mov[arrow][0];
+                ny = y + mov[arrow][1];
             }
 
             if (isBlue(nx, ny)) {
-                ++blueCnt;
-                temp.push_back({ now.x,now.y,chess[now.x][now.y].bottom });
+                continue;
             }
-            else if(isRed(nx, ny)) {
-                if (chess[nx][ny].cnt == 0) {
-                    chess[nx][ny] = { chess[now.x][now.y].top,chess[now.x][now.y].bottom,count };
-                    if (chess[nx][ny].bottom > chess[nx][ny].top) {
-                        info.push({ nx,ny,chess[nx][ny].bottom });
-                    }
-                    else {
-                        temp.push_back({ nx,ny,chess[nx][ny].bottom });
-                    }
-                }
-                else {
-                    chess[nx][ny] = { chess[nx][ny].bottom,chess[now.x][now.y].bottom,chess[nx][ny].cnt + count };
-                }
-                chess[now.x][now.y] = { 0,0,0 };
+            else if (isRed(nx, ny)) {
+                reverse(info[x][y].begin() + idx, info[x][y].end());
+                bokboot(x, y, nx, ny, idx);
             }
             else if (isWhite(nx, ny)) {
-                if (chess[nx][ny].cnt == 0) {
-                    chess[nx][ny] = chess[now.x][now.y];
-                    temp.push_back({ nx,ny,chess[nx][ny].bottom });
-                }
-                else {
-                    chess[nx][ny] = { chess[nx][ny].bottom,chess[now.x][now.y].top,chess[nx][ny].cnt + count };
-                }
-                chess[now.x][now.y] = { 0,0,0 };
+                bokboot(x, y, nx, ny, idx);
             }
-            if (chess[nx][ny].cnt >= 4) {
+            if (info[nx][ny].size() >= 4)
                 return time;
-            }
+
         }
-        if (temp.size() == blueCnt)
+        if (blueCnt == K)
             return -1;
-        for (int a = 0; a < temp.size(); ++a) {
-            info.push({ temp[a].x,temp[a].y,temp[a].idx });
-        }
-        print();
         ++time;
     }
-
     return -1;
 }
 int main() {
@@ -143,7 +118,7 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    freopen("input.txt", "r", stdin);
+    // freopen("input.txt", "r", stdin);
     cin >> N >> K;
     for (int a = 1; a <= N; ++a) {
         for (int b = 1; b <= N; ++b) {
@@ -153,11 +128,10 @@ int main() {
     for (int a = 1; a <= K; ++a) {
         int x, y, arrow;
         cin >> x >> y >> arrow;
-        info.push({x,y,a});
-        chess[x][y] = { a,a,1 };
+        info[x][y].push_back(a);
         arrowMemo[a] = arrow;
+        gps[a] = { x,y };
     }
-    print();
     cout << run();
 
     return 0;
