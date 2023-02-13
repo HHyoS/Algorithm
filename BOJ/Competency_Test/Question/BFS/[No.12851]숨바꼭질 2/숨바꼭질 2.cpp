39144fade1c041648ffe2523c6b4cc8161b28d41
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2023-02-13 22:23:33 +0900

    Update 숨바꼭질 2.cpp

--- BOJ/Competency_Test/Question/BFS/[No.12851]숨바꼭질 2/숨바꼭질 2.cpp
+++ BOJ/Competency_Test/Question/BFS/[No.12851]숨바꼭질 2/숨바꼭질 2.cpp
@@ -1,57 +1,74 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-
-#pragma warning(disable: 4996)
-
+#include <algorithm>
+#include <string>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-bool isCheck[100010];
+struct Node {
+    int idx;
+    int sum;
+};
 
-int main() {
-    int T;
-    int N, M, K;
-    int X, Y;
-    int answer = 0;
-    int answer_count = 0;
-
-    cin >> N >> K;
-    queue<pair<int, int>> q;
-    q.emplace(N, 0);
-
-    while (!q.empty()) {
-        int locate = q.front().first;
-        int cost = q.front().second;
-        q.pop();
-
-        isCheck[locate] = true;
-        isCheck[K] = false;
-
-        if (answer == 0 && locate == K) {
-            answer = cost;
-            answer_count++;
-            continue;
-        }
-
-        if (answer != 0 && answer == cost && locate == K) {
-            answer_count++;
-            continue;
-        }
-
-        if (0 <= locate - 1 && !isCheck[locate - 1])
-            q.emplace(locate - 1, cost + 1);
-
-
-        if (locate + 1 <= K && !isCheck[locate + 1])
-            q.emplace(locate + 1, cost + 1);
-
-
-        if (locate * 2 <= K + 1 && !isCheck[locate * 2])
-            q.emplace(locate * 2, cost + 1);
-
+bool visited[100001];
+int N, K;
+int miniCnt = 0;
+queue<int> que;
+void solve() {
+    if (K <= N) {
+        cout << N-K << "\n" << 1;
+        return;
     }
 
-    cout << answer << endl;
-    cout << answer_count << endl;
+    que.push(N);
+    int time = 1;
+    while (!que.empty()) {
+        int size = que.size();
+        for (int s = 0; s < size; ++s) {
+            int nowIdx = que.front();
+            que.pop();
+            visited[nowIdx] = 1;
+            int backIdx = nowIdx - 1;
+            int goIdx = nowIdx + 1;
+            int jumpIdx = nowIdx * 2;
+
+            if (backIdx >= 0 && visited[backIdx] == 0) {
+                if (backIdx == K)
+                    ++miniCnt;
+                else {
+                    que.push({ backIdx });
+                }
+            }
+            if (goIdx <= K && visited[goIdx]==0) {
+                if (goIdx == K)
+                    ++miniCnt;
+                else {
+                    que.push({ goIdx });
+                }
+            }
+        
+            if (jumpIdx <= 100000 && visited[jumpIdx] == 0) {
+                if (jumpIdx == K)
+                    ++miniCnt;
+                else {
+                    que.push({ jumpIdx });
+                }
+            }
+        }
+        if (miniCnt != 0) {
+            cout << time << "\n" << miniCnt<<"\n";
+            return;
+        }
+        ++time;
+    }
+
+}
+void input() {
+    cin >> N >> K;
+}
+int main() {
+    input();
+    solve();
     return 0;
 }
