1130d45d7085d62603372eaabe285a91caccb9b9
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2023-02-26 21:10:02 +0900

    Update source.cpp

--- BOJ/코테대비/[No.15059]Hard Choice/source.cpp
+++ BOJ/코테대비/[No.15059]Hard Choice/source.cpp
@@ -1,22 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
+#include <queue>
 #include <algorithm>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-int main(void)
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int Ca, Ba, Pa;
-	cin >> Ca >> Ba >> Pa;
+int C, B, P;
+int wantC, wantB, wantP;
+int solve() {
+    int ans = 0;
+    if (wantC > C)
+        ans += (wantC - C);
+    if (wantB > B)
+        ans += (wantB - B);
+    if (wantP > P)
+        ans += (wantP - P);
+    return ans;
+}
+void input() {
+    cin >> C >> B >> P;
+    cin >> wantC >> wantB >> wantP;
+}
 
-	int Cr, Br, Pr;
-	cin >> Cr >> Br >> Pr;
-
-	int result = (Ca - Cr < 0 ? abs(Ca - Cr) : 0)
-		+ (Ba - Br < 0 ? abs(Ba - Br) : 0)
-		+ (Pa - Pr < 0 ? abs(Pa - Pr) : 0);
-
-	cout << result << "\n";
-
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    srand(time(NULL));
+    clock_t start, finish;
+    double duration;
+    freopen("input.txt", "r", stdin);
+    start = clock();
+    input();
+    cout << solve()<<"\n";
+    finish = clock();
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    cout << duration << "초" << endl;
+    return 0;
 }
