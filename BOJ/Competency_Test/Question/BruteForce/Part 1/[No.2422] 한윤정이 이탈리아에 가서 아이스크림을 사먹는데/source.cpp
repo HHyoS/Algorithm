9faddcaa3cbe5fbd8d958f1fc2c67724c243ea87
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2023-02-26 20:55:48 +0900

    Update source.cpp

--- BOJ/Competency_Test/Question/BruteForce/Part 1/[No.2422] 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/source.cpp
+++ BOJ/Competency_Test/Question/BruteForce/Part 1/[No.2422] 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/source.cpp
@@ -1,31 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <unordered_map>
 using namespace std;
- 
+
 int N, M;
-bool chk[205][205];
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
-    cin >> N >> M;
-    for (int i = 0; i < M; ++i)
-    {
-        int p, q;
-        cin >> p >> q;
-        chk[p][q] = chk[q][p] = true;
-    }
-    int ans = 0;
-    for (int i = 1; i <= N; ++i)
-        for (int j = i + 1; j <= N; ++j)
-        {
-            if (chk[i][j]) continue;
-            for (int k = j + 1; k <= N; ++k)
-            {
-                if (chk[i][k] || chk[j][k]) continue;
-                ++ans;
+bool avoid[201][201];
+int solve() {
+    int cnt = 0;
+
+    for (int a = 1; a <= N-2; ++a) {
+        for (int b = a+1; b <= N-1; ++b) {
+            if (avoid[a][b] == 1) continue;
+            for (int c = b+1; c <= N; ++c) {
+                if (avoid[a][c] == 1 || avoid[b][c] == 1)
+                    continue;
+                ++cnt;
             }
         }
-    cout << ans << "\n";
+    }
+    return cnt;
+}
+void input() {
+    cin >> N >> M;
+    for (int a = 0; a < M; ++a) {
+        int first, second;
+        cin >> first >> second;
+        avoid[first][second] = 1;
+        avoid[second][first] = 1;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    srand(time(NULL));
+    clock_t start, finish;
+    double duration;
+    freopen("input.txt", "r", stdin);
+    start = clock();
+    input();
+    cout << solve()<<"\n";
+    finish = clock();
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    cout << duration << "초" << endl;
     return 0;
 }
