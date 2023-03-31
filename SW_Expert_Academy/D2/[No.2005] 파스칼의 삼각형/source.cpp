45ca844741ab28e0b8e03e120c990e216f3c592b
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2023-03-31 23:43:09 +0900

    [Algorithm] SWEA No.2005 파스칼의 삼각형
    난이도 : D2
    문제 유형 : 단순 구현

--- SW_Expert_Academy/D2/[No.2005] 파스칼의 삼각형/source.cpp
+++ SW_Expert_Academy/D2/[No.2005] 파스칼의 삼각형/source.cpp
@@ -1,38 +1,58 @@
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <memory.h>
 using namespace std;
 
-int main(void)
-{
-    int t;
-    int arr[10][10];
+int N;
+int arr[10][10];
+int maxiNum = 0;
+void solve() {
     arr[0][0] = 1;
-    
-    cin >> t;
-    
-    for(int i = 1; i < 10; i++)
-    {
-        for(int j = 0; j <= i; j++)
-        {
-            if(j == 0 || j == i)
-                arr[i][j] = 1;
-            else
-                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+    for (int a = 1; a < N; ++a) {
+        arr[a][0] = 1;
+        for (int b = 1; b < a; ++b) {
+            arr[a][b] = arr[a - 1][b - 1] + arr[a - 1][b];
+        }
+        arr[a][a] = 1;
+    }
+
+}
+void print() {
+    for (int a = 0; a < N; ++a) {
+        for (int b = 0; b <= a; ++b) {
+            cout << arr[a][b] << " ";
+        }
+        cout << "\n";
+    }
+}
+void input() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        cin >> N;
+        cout << "#" << t << "\n";
+        if (maxiNum >= N)
+            print();
+        else {
+            solve();
+            print();
+            maxiNum = N;
         }
     }
-    
-    for(int i = 1; i < t; i++)
-    {
-        int n;
-        cin >> n;
-        
-        cout << "#" << i << endl;
-        for(int j = 0; j < n; j++)
-        {
-            for(int k = 0; k <= j; k++)
-                cout << arr[j][k] << " ";
-            cout << endl;
-        }
-    }
+ }
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+
+    input();
     return 0;
 }
