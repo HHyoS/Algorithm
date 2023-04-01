5b59cfd206ad6c3957b9293ad77b6917248d3465
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2023-04-02 00:13:35 +0900

    [Algorithm] SWEA No.5215 햄버거 다이어트
    난이도 D3
    풀이 유형 : DP
    제출횟수 2회
    Fail 이유 : 배열의 크기 설정 미스

--- SW_Expert_Academy/D3/[No.5215] 햄버거 다이어트/source.cpp
+++ SW_Expert_Academy/D3/[No.5215] 햄버거 다이어트/source.cpp
@@ -1,29 +1,54 @@
-#include<cstdio>
-#include<iostream>
-#include<vector>
-#include<cstring>
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <memory.h>
 using namespace std;
 
-int n, l;
-pair<int, int> p[30];
-int dp[10004];
+int N,L;
+int dp[21][1001];
+int cal[21];
+int happy[21];
+void solve() {
 
-int main() {
-    int t;
-    scanf("%d", &t);
-
-    for (int tc = 1; tc <= t; tc++) {
-        scanf("%d %d", &n, &l);
-
-        memset(dp, 0, sizeof dp);
-        for (int i = 0; i < n; i++) {
-            scanf("%d %d", &p[i].first, &p[i].second);
-
-            for (int j = l; j >= p[i].second; j--) {
-                if (dp[j] < p[i].first + dp[j - p[i].second])
-                    dp[j] = p[i].first + dp[j - p[i].second];
+    for (int a = 1; a <= N; ++a) {
+        for (int b = L; b >= 0; --b){
+            if (b < cal[a]) {
+                dp[a][b] = dp[a - 1][b];
+            }
+            else {
+                dp[a][b] = max(dp[a - 1][b], dp[a - 1][b - cal[a]] + happy[a]);
             }
         }
-        printf("#%d %d\n", tc, dp[l]);
     }
+    int maxi = -1;
+    for (int a = L; a >= 0; --a) {
+        maxi = max(dp[N][a], maxi);
+    }
+    cout << maxi << "\n";
+}
+void input() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        cin >> N >> L;
+        for (int a = 1; a <= N; ++a) {
+            cin >> happy[a] >> cal[a];
+        }
+        solve();
+    }
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+  //  freopen("input.txt", "r", stdin);
+
+    input();
+    return 0;
 }
