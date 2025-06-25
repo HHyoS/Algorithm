67083665514aefdaeb2411eba92511b28f6851d4
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2025-06-25 23:13:00 +0900

    쉬다가 자버릴 뻔

--- BOJ/0x13강 - 이분탐색/[No.18870] 좌표 압축/main.cpp
+++ BOJ/0x13강 - 이분탐색/[No.18870] 좌표 압축/main.cpp
@@ -1,1 +1,30 @@
-Solving...
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    vector<int> coords(N);
+    for (int i = 0; i < N; i++)
+        cin >> coords[i];
+
+    vector<int> temp = coords;
+    sort(temp.begin(), temp.end());
+    temp.erase(unique(temp.begin(), temp.end()), temp.end());
+
+    map<int, int> compressed;
+    for (int i = 0; i < temp.size(); i++)
+        compressed[temp[i]] = i;
+
+    for (int i = 0; i < N; i++)
+        cout << compressed[coords[i]] << ' ';
+    cout << '\n';
+
+    return 0;
+}
