408f94793b1dfceaa2d45e1b1c5b02c4df31b9a4
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2025-06-30 21:58:44 +0900

    Update main.cpp

--- BOJ/0x15강 - 해시/[No.7785] 회사에 있는 사람/main.cpp
+++ BOJ/0x15강 - 해시/[No.7785] 회사에 있는 사람/main.cpp
@@ -1,1 +1,35 @@
-S o l v i ng
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    unordered_set<string> company;
+
+    for (int i = 0; i < n; i++) {
+        string name, status;
+        cin >> name >> status;
+        if (status == "enter") {
+            company.insert(name);
+        } else {
+            company.erase(name);
+        }
+    }
+
+    // 집합을 vector로 복사
+    vector<string> result(company.begin(), company.end());
+    sort(result.rbegin(), result.rend());  // 내림차순 정렬
+
+    for (const string &name : result) {
+        cout << name << '\n';
+    }
+
+    return 0;
+}
